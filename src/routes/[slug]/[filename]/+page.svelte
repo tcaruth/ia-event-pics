4e7b0deb7e35b411839f498e43e90ee14f0497e5
Travis Caruth <tcaruth@gmail.com>
2026-02-05 00:06:20 -0600

    fix button, update config

--- src/routes/[slug]/[filename]/+page.svelte
+++ src/routes/[slug]/[filename]/+page.svelte
@@ -120,10 +120,38 @@
 
 				<div class="action-bar">
 					<button class="action-btn primary" on:click={downloadImage}>
-						<span class="icon">↓</span> Save
+						<span class="icon">
+							<svg
+								fill="currentColor"
+								width="80"
+								height="80"
+								viewBox="0 0 25 24"
+								xmlns="http://www.w3.org/2000/svg"
+								transform="rotate(0 0 0)"
+								><path
+									d="M12.4239 16.75C12.2079 16.75 12.0132 16.6587 11.8763 16.5126L7.26675 11.9059C6.97376 11.6131 6.97361 11.1382 7.26641 10.8452C7.55921 10.5523 8.03408 10.5521 8.32707 10.8449L11.6739 14.1896L11.6739 4C11.6739 3.58579 12.0096 3.25 12.4239 3.25C12.8381 3.25 13.1739 3.58579 13.1739 4L13.1739 14.1854L16.5168 10.8449C16.8098 10.5521 17.2846 10.5523 17.5774 10.8453C17.8702 11.1383 17.87 11.6131 17.5771 11.9059L13.0021 16.4776C12.8646 16.644 12.6566 16.75 12.4239 16.75Z"
+								/><path
+									d="M5.17188 16C5.17188 15.5858 4.83609 15.25 4.42188 15.25C4.00766 15.25 3.67188 15.5858 3.67188 16V18.5C3.67188 19.7426 4.67923 20.75 5.92188 20.75H18.9227C20.1654 20.75 21.1727 19.7426 21.1727 18.5V16C21.1727 15.5858 20.837 15.25 20.4227 15.25C20.0085 15.25 19.6727 15.5858 19.6727 16V18.5C19.6727 18.9142 19.337 19.25 18.9227 19.25H5.92188C5.50766 19.25 5.17188 18.9142 5.17188 18.5V16Z"
+								/></svg
+							>
+						</span>
+						<span>Save</span>
 					</button>
 					<button class="action-btn secondary" on:click={shareImage}>
-						<span class="icon">↗</span> Share
+						<span class="icon"
+							><svg
+								fill="currentColor"
+								width="80"
+								height="80"
+								viewBox="0 0 25 25"
+								xmlns="http://www.w3.org/2000/svg"
+								transform="rotate(0 0 0)"
+								><path
+									d="M15.0928 2.37789C14.8783 2.1634 14.5558 2.09923 14.2755 2.21532C13.9952 2.3314 13.8125 2.60488 13.8125 2.90823V6.75455C7.39629 7.14206 2.3125 12.4683 2.3125 18.982C2.3125 19.8676 2.4066 20.7321 2.58563 21.5657C2.65986 21.9113 2.96538 22.1582 3.31891 22.1582C3.67244 22.1582 3.97796 21.9113 4.05219 21.5657C5.04239 16.955 8.99081 13.4426 13.8125 13.1102V16.9082C13.8125 17.2116 13.9952 17.4851 14.2755 17.6011C14.5558 17.7172 14.8783 17.6531 15.0928 17.4386L22.0929 10.4386C22.2335 10.2979 22.3125 10.1071 22.3125 9.90823C22.3125 9.70932 22.2335 9.51855 22.0929 9.3779L15.0928 2.37789Z"
+								/></svg
+							></span
+						>
+						<span>Share</span>
 					</button>
 				</div>
 
@@ -226,23 +254,25 @@
 		border-radius: var(--radius-md);
 		font-weight: 700;
 		font-size: 1.125rem;
-		border: 2px solid var(--border-color);
+		border: 2px solid var(--text-primary);
 		background: transparent;
 		color: var(--text-surface-primary);
 		transition: var(--transition-base);
 		cursor: pointer;
+		.icon svg {
+			height: 32px;
+			width: 32px;
+		}
 	}
 
 	.action-btn.primary {
 		background: var(--color-primary);
 		color: var(--text-primary);
-		border-color: var(--color-primary);
 	}
 
 	.action-btn.secondary {
 		background: var(--color-secondary);
 		color: var(--text-secondary);
-		border-color: var(--color-secondary);
 	}
 
 	.action-btn:hover {
